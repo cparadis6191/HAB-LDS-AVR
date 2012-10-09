d4ec24c0ece13aa963c2fe1d9ad6cb6ee4829eac
Chad Paradis <cparadis6191@gmail.com>
2012-10-09 14:47:33 -0400

    Deleted two old files (renamed)

--- HAB-LDS-AVR/src/light_direction_sensor.c
+++
@@ -1,9 +0,0 @@
-#include "avr/io.h"
-#include "light_direction_sensor.h"
-
-int main( void ) {
-	while( 1 ) {
-	}
-
-	return 0;
-}
--- HAB-LDS-AVR/src/light_direction_sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DIRECTION_SENSOR_H_
-#define __DIRECTION_SENSOR_H_
-
-#include "bluetooth.h"
-#include "lcd.h"
-#include "sd.h"
-#include "adc.h"
-
-#endif
