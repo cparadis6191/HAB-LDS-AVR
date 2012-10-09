04bc069fc1832c4680cbe46bbaccefbfa3283314
Chad Paradis <cparadis6191@gmail.com>
2012-10-09 14:40:31 -0400

    Imported the project into Atmel Studio 6

---
+++ HAB-LDS-AVR/HAB-LDS-AVR.c
@@ -0,0 +1,17 @@
+/*
+ * HAB_LDS_AVR.c
+ *
+ * Created: 10/9/2012 2:33:16 PM
+ *  Author: Talak
+ */ 
+
+
+#include <avr32/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ HAB-LDS-AVR/HAB-LDS-AVR.h
@@ -0,0 +1,9 @@
+#ifndef __DIRECTION_SENSOR_H_
+#define __DIRECTION_SENSOR_H_
+
+#include "bluetooth.h"
+#include "lcd.h"
+#include "sd.h"
+#include "adc.h"
+
+#endif
